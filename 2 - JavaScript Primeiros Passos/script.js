4337d457520bd4ae4a3704b8b1cc0cf7b05bafbc
GersonST <gersonsevero90@gmail.com>
2021-06-02 21:13:46 -0300

    Revisão 1 JavaScript

---
+++ 2 - JavaScript Primeiros Passos/script.js
@@ -0,0 +1,43 @@
+// função entrar
+
+
+// function entrar() {
+
+//   var area = document.getElementById('area');
+//   var texto = prompt('Digite seu nome:');
+
+//   if(texto == ''){
+//     alert('Digite seu nome novamente');
+//     area.innerHTML = 'Bem vindo ...';
+//   }else{
+//     area.innerHTML = 'Bem vindo ' + texto;
+//   }
+
+// }
+// x = 0;
+
+// while(x < 10){
+// document.write("</br> O valor do x é: " + x);
+
+// x++;
+// }
+
+
+// for (a = 0; a < 10; a++){
+//   document.write("</br> O valor do a é: " + a);
+// }
+
+x = 2;
+
+switch(x){
+
+  case 0:
+    alert("O x vale 0!");
+    break;
+  case 1:
+    alert("O x vale 1!");
+    break;
+  case 2:
+    alert("O x vale 2!");
+    break;
+}
